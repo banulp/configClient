48d6fd169700d2672b6580cbe8b8ef0f35138d55
banulp <banulp@ncsoft.com>
2020-09-29 16:14:56 +0900

    cloud server, client profile 망별 property 접근 되는지 확인

--- src/main/java/com/example/demo/MessageRestController.java
+++ src/main/java/com/example/demo/MessageRestController.java
@@ -3,7 +3,10 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author banulp
@@ -12,11 +15,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 class MessageRestController {
 
-    @Value("${message:'Hello default'}")
+    @Value("${message:'NW'}")
     private String message;
 
+    @Value("${friends:'NW'}")
+    private String friends;
+
+    @Value("${words:'NW'}")
+    private String words;
+
+    @Value("${books:'NW'}")
+    private String books;
+
     @RequestMapping("/message")
-    String getMessage() {
+    public String getMessage() {
         return this.message;
     }
+
+    @RequestMapping("/friends")
+    public String getFriends() {
+        return this.friends;
+    }
+
+    @RequestMapping("/total")
+    public List<String> getTotal() {
+        return List.of("message is " + message, "friends is " + friends, "words is " + words, "books is " + books);
+    }
+
 }
